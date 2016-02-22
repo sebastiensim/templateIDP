788bd1bcf5b6f170566400510b746b7fa50b100f
sebastiensim <wsbastian@gmail.com>
2016-02-22 14:40:53 +0800

    Update SQL

--- template.sql
+++ template.sql
@@ -1,41 +1,78 @@
-CREATE TABLE IF NOT EXISTS `items` (
-  `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+-- phpMyAdmin SQL Dump
+-- version 4.4.10
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 22, 2016 at 07:37 AM
+-- Server version: 5.5.42
+-- PHP Version: 7.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `template`
+--
+CREATE DATABASE IF NOT EXISTS `template` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `template`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `Id` int(10) unsigned NOT NULL,
   `Title` varchar(255) NOT NULL,
   `Description` text NOT NULL,
   `Author` varchar(15) NOT NULL,
   `Image` varchar(255) NOT NULL,
   `Cost` float unsigned NOT NULL DEFAULT '1',
-  `Category` tinyint(3) unsigned NOT NULL,
-  `Tags` text NOT NULL,
-  PRIMARY KEY (`Id`),
-  FULLTEXT KEY `Tags` (`Tags`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+  `Category` tinyint(3) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `requests`
+-- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `requests` (
-  `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `Uid` int(10) unsigned NOT NULL,
-  `Request` text NOT NULL,
-  `State` tinyint(4) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
-  `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `Id` int(10) unsigned NOT NULL,
   `Username` varchar(15) NOT NULL,
   `Password` char(64) NOT NULL,
-  `Rank` tinyint(3) unsigned DEFAULT '0',
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `User` (`Username`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+  `Rank` tinyint(3) unsigned DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `User` (`Username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `Id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
