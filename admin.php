864d9889355aca16dbaee8adb99251c1ce1c5022
zitengwang <wangziteng1218@sina.com>
2016-02-27 11:50:33 +0800

    Add Google Analytics Code

--- admin.php
+++ admin.php
@@ -11,6 +11,16 @@
 	<title>TemPlate</title>
 	<link rel="stylesheet" type="text/css" href="css/includes.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-74388652-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
 <body>
 	<header class="text-center">
